97503f1b2226a7083770ecc89ec16b82f3d68dfe
Patrick Adutwum <patrick.adutwum@protonmail.ch>
2021-02-18 13:24:50 +0000

    CountDownTimer to display seconds

--- app/src/main/java/com/djeneral/a7minutesworkout/Exercise.kt
+++ app/src/main/java/com/djeneral/a7minutesworkout/Exercise.kt
@@ -2,10 +2,14 @@ package com.djeneral.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import com.djeneral.a7minutesworkout.databinding.ExerciseBinding
 
 class Exercise : AppCompatActivity() {
     private lateinit var binding: ExerciseBinding
+
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +23,40 @@ class Exercise : AppCompatActivity() {
         binding.tbExercise.setOnClickListener {
             onBackPressed()
         }
+
+        setupRestView()
+    }
+
+    private fun setRestProgressBar(){
+        binding.progressBar.progress = restProgress
+        restTimer = object : CountDownTimer(10000, 1000){
+            override fun onTick(millisUntilFinished: Long) {
+                restProgress++
+                binding.progressBar.progress = 10 - restProgress
+                binding.tvTimer.text = (10 - restProgress).toString()
+            }
+
+            override fun onFinish() {
+                showToat("Exercise will start now")
+            }
+        }.start()
+    }
+
+    private fun setupRestView(){
+        if (restTimer != null){
+            restTimer!!.cancel()
+            restProgress = 0
+        }
+
+        setRestProgressBar()
+    }
+
+    override fun onDestroy() {
+        if (restTimer != null){
+            restTimer!!.cancel()
+            restProgress = 0
+        }
+
+        super.onDestroy()
     }
 }
